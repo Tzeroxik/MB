70563aba54ebabb2026ae3d154193526df15f12f
Tzeroxik <adriano.halo@gmail.com>
2018-12-30 18:13:24 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 2nd Project for the curricular unit "Modelação Bayesiana"
+
+## Implemented functions
+
+correlation_matrix(mat) 
+* **mat**: the original dataset matrix which is formated as samples in rows and variables in columns.
+* Returns the respective correlation matrix. 
+
+### covariance_matrix(**corrmat**)
+
+ * **corrmat**: the correlated matrix resulting from calling the function **corellation_matrix** on the dataset matrix. 
+* Returns the normalized covariance matrix.
+
+pca(mat) 
+* **mat**: the original dataset matrix which is formated as samples in rows and variables in columns.
+
+* Returns an SVD object (from Julia's LinearAlgebra package) stemming from the single value decomposition of the correlation matrix of **mat**.
--- src/MB.jl
+++ src/MB.jl
@@ -1,10 +1,10 @@
 module MB
-    export center_matrix, cov_center_matrix, pca
+    export correlation_matrix, covariance_matrix, pca
     using LinearAlgebra
     using Statistics
     
-    center_matrix(mat::Array{Float64 ,2})::Array{Float64 ,2} = (mat .- mean(mat,dims=1)) ./ std(mat,dims=1)   
-    cov_center_matrix(centermat::Array{Float64 ,2})::Array{Float64 ,2} = centermat |> length |> float |> n -> centermat' * centermat / (n  - 1.)
-    pca(mat::Array{Float64 ,2})::SVD = mat |> center_matrix |> svd
+    correlation_matrix(mat::Array{Float64 ,2})::Array{Float64 ,2} = (mat .- mean(mat,dims=1)) ./ std(mat,dims=1)   
+    covariance_matrix(corrmat::Array{Float64 ,2})::Array{Float64 ,2} = corrmat |> length |> float |> n -> corrmat' * corrmat / (n  - 1.)
+    pca(mat::Array{Float64 ,2})::SVD = mat |> correlation_matrix |> svd
     
 end 
