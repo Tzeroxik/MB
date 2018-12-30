3e2e32e363b3003c58ba459194884a484f9ad03f
Tzeroxik <adriano.halo@gmail.com>
2018-12-30 15:44:58 +0000

    corrected bug in piping

--- src/MB.jl
+++ src/MB.jl
@@ -1,23 +1,23 @@
 module MB
 
-function covariance_matrix(mat::Array{Float64 ,2})::Float64 
-    out::Array{Float64 ,2} = mat |> size |> zeros
-    
-    local col::Array{Float64 ,2}
-    local dim::Int32
+    function covariance_matrix(mat::Array{Float64 ,2})::Float64 
+        out::Array{Float64 ,2} = mat |> size |> zeros
+        
+        local col::Array{Float64 ,2}
+        local dim::Int32
 
-    for i in axes(mat,1)
-        col = mat[:,i]
-        s = sum(col) / length(col)        
-        out[:,i] = map(x -> x - s, col)
+        for i in axes(mat,1)
+            col = mat[:,i]
+            s = sum(col) / length(col)        
+            out[:,i] = map(x -> x - s, col)
+        end 
+        out
+    end
+
+    function svd(mat::Array{Float64,2})::Float64 
+        mat |> covariance_matrix 
+            |> covmat -> (mat - covmat)' * (mat - covmat) 
+            |> eig
     end 
-    out
-end
-
-function svd(mat::Array{Float64,2})::Float64 
-    mat |> covariance_matrix 
-        |> covmat -> (mat - covmat)' * (mat - covmat) 
-        |> eig
-end 
  
 end 
