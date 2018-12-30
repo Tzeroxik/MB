4f4cab00486437d8ae8cca23c226d863a31e1034
Tzeroxik <adriano.halo@gmail.com>
2018-12-30 17:50:19 +0000

    bug correction

--- src/MB.jl
+++ src/MB.jl
@@ -2,8 +2,8 @@ module MB
     export center_matrix, cov_center_matrix, pca
     using LinearAlgebra
     using Statistics
-
-    center_matrix(mat::Array{Float64 ,2})::Array{Float64 ,2} = (matrix .- mean(mat,dims=1)) ./ std(mat,dims=1)   
+    
+    center_matrix(mat::Array{Float64 ,2})::Array{Float64 ,2} = (mat .- mean(mat,dims=1)) ./ std(mat,dims=1)   
     cov_center_matrix(centermat::Array{Float64 ,2})::Array{Float64 ,2} = centermat |> length |> float |> n -> centermat' * centermat / (n  - 1.)
     pca(mat::Array{Float64 ,2})::SVD = mat |> center_matrix |> svd
     
