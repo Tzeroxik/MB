ccd41bb7bc4bf9f579c9dbfb3cc6bd91146538a2
Tzeroxik <adriano.halo@gmail.com>
2018-12-30 15:37:51 +0000

    corrections

--- src/MB.jl
+++ src/MB.jl
@@ -4,10 +4,11 @@ function covariance_matrix(mat::Array{Float64 ,2})::Float64
     out::Array{Float64 ,2} = mat |> size |> zeros
     
     local col::Array{Float64 ,2}
+    local dim::Int32
 
-    for i in 1:length(mat[:,1])
+    for i in axes(mat,1)
         col = mat[:,i]
-        s = sum(col)        
+        s = sum(col) / length(col)        
         out[:,i] = map(x -> x - s, col)
     end 
     out
