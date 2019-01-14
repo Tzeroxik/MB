87e1e5584e2ff3ee04d813f33db944dbb5004e0e
Tzeroxik <adriano.halo@gmail.com>
2019-01-14 14:51:46 +0000

    added pc selection

--- src/MB.jl
+++ src/MB.jl
@@ -1,10 +1,31 @@
 module MB
-    export correlation_matrix, covariance_matrix, pca
+    export correlation_matrix, covariance_matrix, pca, select_principal_components
     using LinearAlgebra
     using Statistics
-    
+
     correlation_matrix(mat::Array{Float64 ,2})::Array{Float64 ,2} = (mat .- mean(mat,dims=1)) ./ std(mat,dims=1)   
-    covariance_matrix(corrmat::Array{Float64 ,2})::Array{Float64 ,2} = corrmat |> length |> float |> n -> corrmat' * corrmat / (n  - 1.)
-    pca(mat::Array{Float64 ,2})::SVD = mat |> correlation_matrix |> svd
     
+    covariance_matrix(corrmat::Array{Float64 ,2})::Array{Float64 ,2} = corrmat |> length |> float |> n -> corrmat' * corrmat / (n  - 1.)
+
+    pca(mat::Array{Float64 ,2})::SVD = mat |> correlation_matrix |> svd
+
+    AUTOMATIC = 0. 
+
+    function select_principal_components((U,S,_)::SVD; ncomponents::Float64 = AUTOMATIC)::Array{Float64 ,2}
+        
+        if ncomponents != AUTOMATIC
+            for i in axes(U,2)
+                if norm(U[:,i]) <= 1.
+                    ncomponents = i
+                    break
+                end
+            end
+        end
+        K = 1:ncomponents
+
+        U[:,K] * S[K,K]
+    end
+
+
+
 end 
